52854ea9a9305d7653c78c7207877069e7c32422
Tanya <test@mail.ru>
2024-06-03 21:29:28 +0300

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+//startBtn
+//cards
+//timeController
+//modal
+//board
+//time
+//scoreE1
+
+const startBtn = document.getElementById("start");
+console.log(startBtn);
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+const time = document.getElementById("time");
+console.log(time);
+const modal = document.getElementById("modal");
+console.log(modal);
+const board = document.getElementById("board");
+console.log(board);
+const timeController = document.getElementById("time-list");
+console.log(timeController);
+const scoreE1 = document.getElementById("score");
+console.log(scoreE1);
